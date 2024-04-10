1d20f0fd9f4a531059fa5caae65eaefa11d72bef
bS-ui <brandonxsears@gmail.com>
2024-04-10 16:19:44 -0400

    Part 1

---
+++ data/skill-data.js
@@ -0,0 +1,10 @@
+const skills = [
+  {description: 'JavaScript', skillLevel: 6},
+  {description: 'HTML', skillLevel: 5},
+  {description: 'CSS', skillLevel: 6},
+  {description: 'Python', skillLevel: 6},
+]
+
+export { 
+	skills
+}
